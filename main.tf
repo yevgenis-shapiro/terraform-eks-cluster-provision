05bdc309e37802d8745c919bd5da7bec6c07f094
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-15 02:38:05 +0300

    update

--- main.tf
+++ main.tf
@@ -35,6 +35,14 @@ provider "kubernetes" {
   }
 }
 
+# Helm provider
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+}
 
 data "aws_availability_zones" "available" {
   state = "available"
