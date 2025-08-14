8911249cfea8996f1d9c1917922c9c206cc44b76
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-15 01:07:25 +0300

    update

--- main.tf
+++ main.tf
@@ -31,23 +31,6 @@ provider "kubernetes" {
   }
 }
 
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-  default     = "my-eks-cluster"
-}
-
-variable "cluster_version" {
-  description = "Kubernetes version to use for the EKS cluster"
-  type        = string
-  default     = "1.28"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
 
 data "aws_availability_zones" "available" {
   state = "available"
