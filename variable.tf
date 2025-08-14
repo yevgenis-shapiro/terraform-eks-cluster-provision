e42ec443c4a3a01efc96ccc0f2ba8c61c1f0bd63
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-15 01:22:25 +0300

    update

--- variable.tf
+++ variable.tf
@@ -2,13 +2,13 @@
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "my-eks-cluster"
+  default     = "eks-cluster"
 }
 
 variable "cluster_version" {
   description = "Kubernetes version to use for the EKS cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.31"
 }
 
 variable "vpc_cidr" {
