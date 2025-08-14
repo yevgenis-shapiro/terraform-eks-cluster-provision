d1071969533cecb9cea7fa2ffb87e3b2f034b73e
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-15 01:18:56 +0300

    update

--- data.tf
+++ data.tf
@@ -92,23 +92,4 @@ resource "aws_iam_openid_connect_provider" "eks" {
   url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
 }
 
-# Outputs
-output "cluster_endpoint" {
-  value = aws_eks_cluster.main.endpoint
-}
 
-output "cluster_security_group_id" {
-  value = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
-}
-
-output "cluster_oidc_issuer_url" {
-  value = aws_eks_cluster.main.identity[0].oidc[0].issuer
-}
-
-output "cluster_certificate_authority_data" {
-  value = aws_eks_cluster.main.certificate_authority[0].data
-}
-
-output "cluster_name" {
-  value = aws_eks_cluster.main.name
-}
