6750463a3cc5cb799e49ee770d73e22c3e805cc8
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-15 02:36:53 +0300

    update

--- main.tf
+++ main.tf
@@ -9,6 +9,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0"
